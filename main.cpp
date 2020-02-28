ec9c8c59f9be98e113e2bb7b62376026597efb6a
Penguin Grape <akakiidvachevskiy@gmail.com>
2020-02-28 20:54:11 +0300

    heapsort ready

--- main.cpp
+++ main.cpp
@@ -1,20 +1,19 @@
-#define _CRT_SECURE_NO_WARNINGS
+//#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#define _fseeki64 fseeko64
+#define _ftelli64 ftello64
 using namespace std;
 
 
-int* arr;
-char** strings;
-void quicksort(int start, int end);
-bool check_string(const char* inp);
-
-
-/* как показывает практика - так почему-то дольше чем 2 массива
- * полагаю из-за обращений к памяти, чтобы еще раз обратиться к памяти (arr[index].FIELD)
 struct element {
     long value;
     char* string;
-};*/
+};
+
+
+bool check_string(const char* inp);
+void heapify(element* arr, int count, int root);
+
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -34,7 +33,7 @@ int main(int argc, char *argv[]) {
     if (fread(filecontent, filesize, 1, fp) != 1) {
         return 2;
     }
-    count = strtol(filecontent, nullptr, 10);
+    count = (int)strtol(filecontent, nullptr, 10);
     if (count == 0){
         if (filecontent[0] == '0') {
             return 0;
@@ -42,19 +41,18 @@ int main(int argc, char *argv[]) {
             return 3;
         }
     }
-    arr = new int [count];
-    strings = new char* [count];
+    auto *arr = new element [count];
     int index = 0;
     for (size_t i = 0; i < filesize; i++) {
         if (filecontent[i] == '\n') {
             filecontent[i] = '\0';
             if (index < count) {
-                strings[index] = filecontent + i + 1;
-                if (check_string(strings[index])) {
+                arr[index].string = filecontent + i + 1;
+                if (check_string(arr[index].string)) {
                     return 3;
                 }
-                arr[index] = (int)strtol(strings[index], nullptr, 10);
-                if (arr[index] == 0 && strings[index][0] != '0') {
+                arr[index].value = (int)strtol(arr[index].string, nullptr, 10);
+                if (arr[index].value < 1 && arr[index].string[0] != '0') {
                     return 3;
                 }
                 index++;
@@ -64,7 +62,19 @@ int main(int argc, char *argv[]) {
     if (index < count) {
         return 3;
     }
-    quicksort(0, count - 1);
+
+
+    for (int i = count / 2 - 1; i >= 0; i--) {
+        heapify(arr, count, i);
+    }
+    for (int i= count - 1; i >= 0; i--)
+    {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+    for (int i = 0; i < count; i++) {
+        cout << arr[i].value << endl;
+    }
     FILE *fpw;
     fpw = fopen(argv[2], "wb");
     if (fpw == nullptr){
@@ -74,42 +84,11 @@ int main(int argc, char *argv[]) {
         return 2;
     }
     for (int i = 0; i < count; i++) {
-        if (fprintf(fpw, "%s\n", strings[i]) < 0) {
+        if (fprintf(fpw, "%s\n", arr[i].string) < 0) {
             return 2;
         }
     }
     return 0;
-}
-
-
-void quicksort(int start, int end) {
-    int l = start;
-    int r = end;
-    int pivot = arr[(l + r) / 2];
-    while (l <= r){
-        while (arr[l] < pivot) {
-            l-=-1;
-        }
-        while (arr[r] > pivot) {
-            r+=-1;
-        }
-        if (l <= r){
-            int tmp = arr[l];
-            char *stmp = strings[l];
-            arr[l] = arr[r];
-            strings[l] = strings[r];
-            arr[r] = tmp;
-            strings[r] = stmp;
-            l-=-1;
-            r+=-1;
-        }
-    }
-    if (r > start){
-        quicksort(start, r);
-    }
-    if (l < end){
-        quicksort(l, end);
-    }
 }
 
 
@@ -133,4 +112,20 @@ bool check_string(const char* inp) {
         i++;
     }
     return false;
+}
+
+
+void heapify(element* arr, int count, int root) {
+    int max = root;
+    int l = 2 * root + 1;
+    int r = 2 * root + 2;
+    if (l < count && arr[l].value > arr[max].value)
+        max = l;
+    if (r < count && arr[r].value > arr[max].value)
+        max = r;
+    if (max != root)
+    {
+        swap(arr[root], arr[max]);
+        heapify(arr, count, max);
+    }
 }
